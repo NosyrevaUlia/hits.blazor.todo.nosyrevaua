f49486c068681d945e9ce1e7947d459f51be1702
Ulia Nosyreva <ulianosyreva752@gmail.com>
2024-11-23 01:12:36 +0500

    осталось задание

--- TodoServerApp/Data/Services/MemoryDataService.cs
+++ TodoServerApp/Data/Services/MemoryDataService.cs
@@ -10,10 +10,21 @@ namespace TodoServerApp.Data.Services
             new () {Id = 2, Title="Task", Description = "Task 2 Description", CreatedDate=DateTime.Now},
             new () {Id = 3, Title="Task", Description = "Task 3 Description", CreatedDate=DateTime.Now},
         ];
-        public async Task<IEnumerable<TaskItem>> GetTaskItemsAsync()
+
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<TaskItem>> GetAllAsync()
         {
             await Task.Delay(1000);
             return await Task.FromResult(Tasks);
+        }
+
+        public Task<TaskItem> GetTaskAsync(int id)
+        {
+            throw new NotImplementedException();
         }
 
         public Task SaveAsync(TaskItem item)
--- TodoServerApp/Data/TaskItem.cs
+++ TodoServerApp/Data/TaskItem.cs
@@ -3,9 +3,14 @@
     public class TaskItem
     {
         public int Id { get; set; }
-        public string? Title { get; set; } 
+        public string? Title { get; set; }
         public string? Description { get; set; }
         public DateTime? CreatedDate { get; set; }
         public DateTime? FinishDate { get; set; }
     }
+    
 }
+
+
+// завести справочник пользвателя с возможностью создание/изменение/удаление ФИО (слева пользователи в меню)
+// модифицировать сущность задачи (исполнитель в задачи(справочник))
